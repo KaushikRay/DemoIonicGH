1fad1a9a787957912e7538da6d03b154ce34881c
Kaushik Ray <kr.ray.kaushik@gmail.com>
2016-08-03 11:21:04 +0200

    basic pop up coming with half screen on click marker

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -84,11 +84,11 @@ angular.module('starter.controllers', ['forceng'])
 
     })
 
-    .controller('AccountMapCtrl', function ($scope, $stateParams, force) {
+    .controller('AccountMapCtrl', function ($scope, $stateParams, force, $ionicModal) {
         console.log('hello --->');
     })
 
-    .controller('MyLocationCtrl', function($scope, $stateParams, force, $cordovaGeolocation) {
+    .controller('MyLocationCtrl', function($scope, $stateParams, force, $cordovaGeolocation, $ionicModal) {
         console.log('this is in my location page');
         var options = {timeout: 10000, enableHighAccuracy: true};
         $cordovaGeolocation.getCurrentPosition(options).then(function(position){
@@ -103,7 +103,71 @@ angular.module('starter.controllers', ['forceng'])
 
             $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
+            //Wait until the map is loaded
+            google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+             
+              var marker = new google.maps.Marker({
+                  map: $scope.map,
+                  animation: google.maps.Animation.DROP,
+                  position: latLng
+              });
+
+              $scope.contacts = [
+                { name: 'Gordon Freeman' },
+                { name: 'Barney Calhoun' },
+                { name: 'Lamarr the Headcrab' },
+              ];
+
+              // $ionicModal.fromTemplateUrl('templates/modal.html', {
+              //   scope: $scope
+              // }).then(function(modal) {
+              //   $scope.modal = modal;
+              //   marker.addListener('click', function() {
+              //     console.log('helllos from marker');
+              //     modal.show();
+              //   });
+              // });
+              $ionicModal.fromTemplateUrl('templates/bottom-sheet.html', {
+                scope: $scope,
+                viewType: 'bottom-sheet',
+                animation: 'slide-in-up'
+              }).then(function(modal) {
+                $scope.modal = modal;
+                marker.addListener('click', function() {
+                  console.log('helllos from marker');
+                  modal.show();
+                });
+              });
+              
+              $scope.createContact = function(u) {        
+                $scope.contacts.push({ name: u.firstName + ' ' + u.lastName });
+                $scope.modal.hide();
+                console.log('$scope.contacts -->', $scope.contacts);
+              }; 
+             
+            });
+
         }, function(error){
             console.log("Could not get location");
         });
-    });
+
+    })
+
+
+    .directive('ionBottomSheet', [function() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      replace: true,
+      controller: [function() {}],
+      template: '<div class="modal-wrapper" ng-transclude></div>'  
+    };
+  }])
+.directive('ionBottomSheetView', function() {
+  return {
+    restrict: 'E',
+    compile: function(element) {
+      element.addClass('bottom-sheet modal');
+    }
+  };
+});
